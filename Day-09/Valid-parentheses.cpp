c32bf499556edbe9e246e148d1abab80778a0d5e
Sakshi Saraswat <128903067+sakshisaraswat09@users.noreply.github.com>
2024-08-09 01:49:12 +0530

    Create Valid-parentheses.cpp

---
+++ Day-09/Valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }else if(!st.empty() && st.top() == '(' && s[i]==')'){
+                st.pop();
+            }else if(!st.empty() && st.top() == '[' && s[i]==']'){
+                st.pop();
+            }else if(!st.empty() && st.top() == '{' && s[i]=='}'){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+        if(st.size()==0) return true;
+        return false;
+    }
+};
