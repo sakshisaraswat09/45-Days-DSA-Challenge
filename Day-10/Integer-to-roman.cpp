c7a00a549d802402858d1e6f50b3cc4e5a5515a3
Sakshi Saraswat <128903067+sakshisaraswat09@users.noreply.github.com>
2024-08-10 02:07:45 +0530

    Create Integer-to-roman.cpp

---
+++ Day-10/Integer-to-roman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        int n[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string s[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int i=0;
+        string ans="";
+        while(num){
+            if(num>=n[i]){
+                int d = num/n[i];
+                while(d--){
+                    ans+=s[i];
+                }
+                num=num%n[i];
+                i++;
+            }else{
+                i++;
+            }
+        }
+        return ans;
+    }
+};
