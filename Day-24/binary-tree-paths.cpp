dba01963f1ad64a83d564debdab029be703cb5c0
Sakshi Saraswat <128903067+sakshisaraswat09@users.noreply.github.com>
2024-08-25 02:04:46 +0530

    Create binary-tree-paths.cpp

---
+++ Day-24/binary-tree-paths.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root,vector<string>& ans,string path){
+        if(!root){
+            return;
+        }
+        if(!root->left && !root->right){
+            ans.push_back(path+"->"+to_string(root->val));
+            return;
+        }
+        
+        solve(root->left,ans,path+"->"+to_string(root->val));
+        solve(root->right,ans,path+"->"+to_string(root->val));
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>ans;
+        if(!root) return ans;
+        string path = to_string(root->val);
+        solve(root->left,ans,path);
+        solve(root->right,ans,path);
+        if(ans.empty()) ans.push_back(path);
+        return ans;
+    }
+};
